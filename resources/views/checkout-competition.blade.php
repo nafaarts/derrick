de34055daed2b6d278de4ff3981f9a31b0b6965b
Nafaarts <71387948+nafaarts@users.noreply.github.com>
2022-07-05 14:48:31 +0700

    fix fe duitku #2

--- resources/views/checkout-competition.blade.php
+++ resources/views/checkout-competition.blade.php
@@ -61,12 +61,12 @@
         defer></script>
 
     </script>
-    <script>
+    {{-- <script>
         window.jQuery || document.write(
                 `<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
                 integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
     </script>`)
-    </script>
+    </script> --}}
 @endsection
 
 @section('scripts')
