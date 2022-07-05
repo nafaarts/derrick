49c2b4c1f34192aac022e07d1712e9b8e3f2cec0
Nafaarts <71387948+nafaarts@users.noreply.github.com>
2022-07-05 14:30:40 +0700

    add jquery

--- resources/views/checkout-competition.blade.php
+++ resources/views/checkout-competition.blade.php
@@ -55,6 +55,9 @@
 @endsection
 
 @section('head')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"
+        integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script
         src="{{ env('PAYMENT_IS_PRODUCTION') ? 'https://app-prod.duitku.com/lib/js/duitku.js' : 'https://app-sandbox.duitku.com/lib/js/duitku.js' }}"
         defer></script>
